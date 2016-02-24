a05065e9e84120a207cb2a4ab02ab63d7eb20139
kargok <karenrgo@gmail.com>
2016-02-24 00:09:29 -0500

    Update complete.R
    Added clarifying comments and removed some unneccessary code

--- complete.R
+++ complete.R
@@ -14,18 +14,22 @@ complete <- function(directory, id = 1:332) {
     ## number of complete cases
     
     library(stringr)
+    ## loads a library to properly pad the id numbers into the file format
     nobs = rep(0, length(id))
     
     for(i in seq_along(id)) {
         
         monitorid = str_pad(id[i], 3, side = "left", pad = "0")
-        monitorid
         filename = paste(directory, "/", monitorid, ".csv", sep = "")
         rawdata = read.csv(filename, header = TRUE)
+        ## reads the CSV files
         
         nobs_i = sum(complete.cases(rawdata))
+        ## counts the number of complete cases within the file
         nobs[i] = nobs_i
+        ## stores the count into the initialized vector
         
     }
     data.frame(id,nobs)
+    ## combines the id and the number of complete cases into a data frame
 }
